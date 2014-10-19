ba82c0f3c8cea5e3ca85bb74f8a669b77327e72d
Danse Macabre <danse616@gmail.com>
2014-10-19 15:46:35 +0400

    remove automatic comments from tracking function

--- core/tracking.py
+++ core/tracking.py
@@ -15,7 +15,6 @@ def start_track(start, track, types_to_track, **kwargs):
 
 
 def _do_track(start, track, types_to_track, skip_functions, **kwargs):
-    leave_comments = kwargs.get('leave_comments', False)
     allow_members = kwargs.get('allow_members', False)
 
     try:
@@ -42,25 +41,16 @@ def _do_track(start, track, types_to_track, skip_functions, **kwargs):
                 if item[1].reg in rsp:
                     rsp.append(item[0].reg)
                 _update_track(track, item[0].reg, track.get(item[1].reg))
-                if leave_comments:
-                    _make_comment(track, item, item[0].reg,
-                                  track.get(item[1].reg))
 
             # mov o_mem, o_reg
             elif item[0].type == o_mem and item[1].type == o_reg:
                 if item[1].reg in track:
                     _update_track(track, Pointer(item[0].value), item[1].reg)
-                    if leave_comments:
-                        _make_comment(track, item, Pointer(item[0].value).name,
-                                      track[item[1].reg])
 
             # mov o_reg, o_mem
             elif item[0].type == o_reg and item[1].type == o_mem:
                 if Pointer in types_to_track:
                     _update_track(track, item[0].reg, Pointer(item[1].value))
-                    if leave_comments and Pointer(item[1].value).type != "":
-                        _make_comment(track, item, item[0].reg,
-                                      Pointer(item[1].value).type)
 
             # mov o_reg, [o_displ|o_phrase]
             elif item[0].type == o_reg and item[1].type in [o_displ, o_phrase]:
@@ -69,16 +59,12 @@ def _do_track(start, track, types_to_track, skip_functions, **kwargs):
                 if item[1].reg in rsp:
                     lvar = find_object(function.frame, name=item[1].displ_str)
                     _update_track(track, item[0].reg, lvar)
-                    if leave_comments:
-                        _make_comment(track, item, item[0].reg, lvar)
                 if allow_members and item[1].reg in track and \
                         isinstance(track[item[1].reg], Structure) and \
                         StructureMember in types_to_track:
                     member = find_object(track[item[1].reg].members(),
                                          offset=item[1].displ)
                     _update_track(track, item[0].reg, member)
-                    # if leave_comments:
-                    #     _make_comment(track, item, item[0].reg, member)
 
             # mov [o_displ|o_phrase], o_reg
             elif item[0].type in [o_displ, o_phrase] and item[1].type == o_reg:
@@ -86,8 +72,6 @@ def _do_track(start, track, types_to_track, skip_functions, **kwargs):
                     lvar = find_object(function.frame, name=item[0].displ_str)
                     if lvar is not None:
                         _update_track(track, lvar, item[1].reg)
-                        if leave_comments:
-                            _make_comment(track, item, lvar, track.get(item[1].reg))
 
             elif item[0].type == o_reg:
                 _update_track(track, item[0].reg, None)
