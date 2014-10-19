901c4071389874686bb73ca701081c57b2313933
Danse Macabre <danse616@gmail.com>
2014-10-19 16:48:01 +0400

    add workarounds for some trivial cases that can be encountered while tracking

--- core/tracking.py
+++ core/tracking.py
@@ -112,7 +112,7 @@ def _do_track(start, track, types_to_track, skip_functions, **kwargs):
                 _update_track(track, item[0].reg,
                               ImmediateValue(item[1].value))
 
-            # mov [o_displ|o_phrase], o_reg
+            # mov [o_displ|o_phrase], o_imm
             elif item[0].type in [o_displ, o_phrase] and item[1].type == o_reg:
                 if item[0].reg in rsp:
                     lvar = find_object(function.frame, name=item[0].displ_str)
@@ -129,7 +129,7 @@ def _do_track(start, track, types_to_track, skip_functions, **kwargs):
             if item[0].type in [o_displ, o_phrase]:
                 if item[0].reg in rsp:
                     lvar = find_object(function.frame, name=item[0].displ_str)
-                    if lvar in track:
+                    if lvar is not None:
                         _update_track(track, lvar, EffectiveAddr(item[1]))
 
         elif item.mnem == 'call':
@@ -146,6 +146,23 @@ def _do_track(start, track, types_to_track, skip_functions, **kwargs):
 
         elif item.mnem == 'jmp':
             has_jumps = True
+
+        # Workarounds for some special trivial cases
+        else:
+
+            if item.mnem == 'xor' and item[0].type == o_reg and \
+                    item[1].type == o_reg:
+                _update_track(track, item[0].reg, ImmediateValue(0))
+
+            if item.mnem == 'and' and item[1].type == o_imm and \
+                    item[1].value == 0:
+                if item[0].type == o_reg:
+                    _update_track(track, item[0].reg, 0)
+                elif item[0].type in [o_displ, o_phrase]:
+                    if item[0].reg in rsp:
+                        lvar = find_object(function.frame, name=item[0].displ_str)
+                        if lvar is not None:
+                            _update_track(track, lvar, ImmediateValue(0))
 
         if not (die_hard or any(map(lambda x: x.__class__ in
                                     types_to_track, track))):
